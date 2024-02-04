213eca25bdd4693b5eb21cd19e10bbea91f9d85a
jitenderyadavofc <157213132+jitenderyadavofc@users.noreply.github.com>
2024-02-04 15:48:08 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,12 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
+    AccountEmail              = "jitender+tools@gmail.com"
+    AccountName               = "tools account"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    SSOUserEmail              = "jitender+tools@gmail.com"
+    SSOUserFirstName          = "tools"
+    SSOUserLastName           = "account"
   }
 
   account_tags = {
