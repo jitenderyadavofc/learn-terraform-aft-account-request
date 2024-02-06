4ee87ed55b11b59dd70ea223265f0ba86a8498a5
jitenderyadavinogen <jitender@birlasoft.com>
2024-02-06 11:04:54 +0530

    added account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,21 @@
-module "tools-01" {
+module "toolsbirla" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "jitender+tools1@gmail.com"
-    AccountName               = "tools1 account"
-    ManagedOrganizationalUnit = "workload"
-    SSOUserEmail              = "jitender+tools1@gmail.com"
-    SSOUserFirstName          = "tools1"
+    AccountEmail              = "jitender+tools4@birlasoft.com"
+    AccountName               = "tools4 account"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "jitender+tools4@birlasoft.com"
+    SSOUserFirstName          = "tools4"
     SSOUserLastName           = "account"
   }
 
   account_tags = {
-    "Learn Tutorial" = "latest"
+    "Learn Tutorial" = "latest test"
   }
 
   change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
+    change_requested_by = "HashiCorp Learn check"
     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
   }
 
